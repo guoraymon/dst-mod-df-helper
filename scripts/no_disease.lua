ca1c4e3def510ac07ead3521b36899528f0d2ed6
raymon <raymon.guo@outlook.com>
2020-04-28 00:42:26 +0800

    fix 草蜥蜴、疾病 nil bug

--- scripts/no_disease.lua
+++ scripts/no_disease.lua
@@ -1,24 +1,23 @@
 local modmastersim = GLOBAL.TheNet:GetIsMasterSimulation()
 
- -- 系统设置无疾病
-TUNING.DISEASE_CHANCE = 0
-TUNING.DISEASE_DELAY_TIME = 0
-TUNING.DISEASE_DELAY_TIME_VARIANCE = 0
+local SpawnPrefab = GLOBAL.SpawnPrefab
+local TUNING = GLOBAL.TUNING
+
+-- no more grass morphing
+TUNING.GRASSGEKKO_MORPH_CHANCE = 0
 
 if modmastersim then
-	-- 治好患病植物
+	-- cure stuff if there is something diseased
 	local function TurnIntoNormal(inst)
 		local x, y, z = inst.Transform:GetWorldPosition()
 		local normal = SpawnPrefab(inst.prefab)
 		normal.Transform:SetPosition(x, y, z)
 		inst:Remove()
-    end
-    
+	end
 	local function DelayCure(self)
 		if self:IsDiseased() or self:IsBecomingDiseased() then
 			self.inst:DoTaskInTime(0, TurnIntoNormal)
 		end
-    end
-    
-    AddComponentPostInit("diseaseable", DelayCure)
+	end
+	AddComponentPostInit("diseaseable", DelayCure)
 end
--- scripts/no_grassgekko.lua
+++ scripts/no_grassgekko.lua
@@ -1,20 +1,21 @@
 local modmastersim = GLOBAL.TheNet:GetIsMasterSimulation()
 
- -- 系统设置无草蜥蜴
+local SpawnPrefab = GLOBAL.SpawnPrefab
+local TUNING = GLOBAL.TUNING
+
+-- no more grass morphing
 TUNING.GRASSGEKKO_MORPH_CHANCE = 0
 
 if modmastersim then
-    -- 草蜥蜴变草
-    local function TurnIntoGrass(inst)
-        local x, y, z = inst.Transform:GetWorldPosition()
-        local grass = SpawnPrefab("grass")
-        grass.Transform:SetPosition(x, y, z)
-        inst:Remove()
-    end
-
-    local function DelaySwap(inst)
-        inst:DoTaskInTime(0, TurnIntoGrass)
-    end
-
-    AddPrefabPostInit("grassgekko", DelaySwap)
+	-- gekkos into grass
+	local function TurnIntoGrass(inst)
+		local x, y, z = inst.Transform:GetWorldPosition()
+		local grass = SpawnPrefab("grass")
+		grass.Transform:SetPosition(x, y, z)
+		inst:Remove()
+	end
+	local function DelaySwap(inst)
+		inst:DoTaskInTime(0, TurnIntoGrass)
+	end
+	AddPrefabPostInit("grassgekko", DelaySwap)
 end
